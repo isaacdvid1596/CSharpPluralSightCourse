4de9f52f32056c48857a80c06311e0cd71ac083f
Julio Isaac David Ramos <71915930+jisaacdavid@users.noreply.github.com>
2021-06-12 17:20:59 -0600

    Add files via upload

---
+++ LINQ fundamentals/LinqSamples/Introduction/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+            ShowLargeFilesWithoutLinq(path);
+            Console.WriteLine("*****************");
+            ShowLargeFilesWithLinq(path);
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            //var query = from file in new DirectoryInfo(path).GetFiles()
+            //            orderby file.Length descending
+            //            select file;
+
+            var query = new DirectoryInfo(path).GetFiles()
+                            .OrderByDescending(f => f.Length)
+                            .Take(5);
+
+            foreach(var file in query)
+            {
+
+                Console.WriteLine($"{file.Name}:{file.Length}");
+            }
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] files = dir.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+
+            for(int i=0;i<5;i++)
+            {
+                FileInfo file = files[i];
+                Console.WriteLine($"{file.Name}:{file.Length}");
+            }
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare([AllowNull] FileInfo x, [AllowNull] FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
